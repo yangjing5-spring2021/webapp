6b34ca7977a3f238048099d8c90ca129cd0a25f9
Jing Yang <58899117+JY-5@users.noreply.github.com>
2021-02-18 03:05:49 -0800

    submit assignment

--- routes/books-route.js
+++ routes/books-route.js
@@ -1,28 +1,18 @@
 'use strict';
 const router = require('express').Router();
-const Book = require('../models/book');
-const userData = require('../userData');
+const models = require('../models/models');
+const userData = require('./userData');
 
 router.post('/', function (req, res) {
     const authorization = req.headers.authorization;
-    //console.log(req.body);
     const { title, author, isbn, published_date } = req.body;
 
     userData.authenticateUser(authorization)
         .then((authResult) => {
-            //console.log("book0");
-            // Book.addBook(book, result.userInfo.id)
-            // .then((addedBook) => {
-            //     res.status(201).json(addedBook);
-            // })
-            // .catch((err) => {
-            //     Promise.reject(err);
-            // })
             if (title && author && isbn && published_date) {
-                    //console.log("book1");
                     const { v4: uuidv4 } = require('uuid');
                     const uid = uuidv4();
-                    Book.create({
+                    models.Book.create({
                         id: uid,
                         title: title,
                         author: author,
@@ -31,28 +21,21 @@ router.post('/', function (req, res) {
                         book_created: new Date(),
                         user_id: authResult.userInfo.id
                     }).then((addedBook) => {
-                        //console.log(addedBook);
-                        // if (data) {
-                        //     return data.get({ plain: true });
-                        // }
                         res.status(201).json(addedBook);
                     }).catch((err) => {
-                        //console.log("book1 catch");
                         res.status(500).json({error : err});
                     });   
             } else {
-                //console.log("books2");
                 res.status(400).json({error : "Incomplete info"});
             }
         }).catch((err) => {
-            //console.log("aaa");
             res.status(401).json({error : err});
         })   
 });
 
 router.get('/:id', function(req, res) {
     const bookId = req.params.id;
-    Book.findOne({
+    models.Book.findOne({
         where: {
             id: bookId
         }
@@ -74,17 +57,15 @@ router.delete('/:id', function(req, res) {
     userData.authenticateUser(authorization)
         .then((authResult) => {
             const userId = authResult.userInfo.id;
-            Book.findOne({
+            models.Book.findOne({
                 where: {
                     id: bookId
                 }
             }).then((data) => {
                 if (data) {
                     data = data.get({ plain: true });
-                    console.log(data);
-                    console.log(userId);
                     if (data.user_id === userId) {
-                        Book.destroy({
+                        models.Book.destroy({
                             where: {
                                 id: bookId
                             }
@@ -96,7 +77,6 @@ router.delete('/:id', function(req, res) {
                             res.status(500).json({error : err});
                         }); 
                     } else {
-                        console.log("401");
                         res.status(401).json({});
                     }
                 } else {
@@ -111,7 +91,7 @@ router.delete('/:id', function(req, res) {
 })
 
 router.get('/', function(req, res) {
-    Book.findAll({})
+    models.Book.findAll({})
     .then((data) => {
         res.status(200).json(data);
     }).catch((err) => {
