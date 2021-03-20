2492a084adc09b5070047950fcafe38067c48b3b
Jing Yang <58899117+JY-5@users.noreply.github.com>
2021-03-19 20:38:46 -0700

    env

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -5,7 +5,8 @@ echo "application start"
 # actually start the server
 # sudo pm2 start /home/ubuntu/webapp/server.js -i 0 --name "web-app"
 cd /home/ubuntu/webapp
-sudo source /etc/profile
+# sudo source /etc/profile
+mv /home/ubuntu/.env /home/ubuntu/webapp/.env
 # sudo nohup node server.js > /dev/null 2> /dev/null < /dev/null &
 sudo forever start server.js
 echo "app started"
